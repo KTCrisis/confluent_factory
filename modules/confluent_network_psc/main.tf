6ed013017254bc761ab65e88b2be653d66caeac5
KTCrisis <ktcrisis@gmail.com>
2026-03-04 11:11:55 +0100

    Update main.tf

--- modules/confluent_network_psc/main.tf
+++ modules/confluent_network_psc/main.tf
@@ -22,35 +22,3 @@ resource "confluent_network" "private-service-connect" {
   #   prevent_destroy = true
   # }
 }
-
-
-# resource "confluent_private_link_access" "gcp_private_link_access" {
-#   for_each = { for connection in var.connection_list : connection.connection_name => connection_ }
-
-#   display_name = each.value.connection_name
-#   gcp {
-#     project = each.value.connection_project_id
-#   }
-#   environment {
-#     id = data.confluent_environment.environment.id
-#   }
-#   network {
-#     id = confluent_network.private-service-connect[0].id
-#   }
-# }
-
-# resource "confluent_network" "peering" {
-#   count = var.connection_types == "PEERING" ? 1: 0
-#   display_name     = var.confluent_kafka_network
-#   cloud            = var.cloud_provider
-#   region           = var.region
-#   cidr             = var.cidr
-#   connection_types = ["PEERING"]
-#   environment {
-#     id = data.confluent_network.environment.id
-#   }
-
-# lifecycle {
-#   prevent_destroy = true
-# }
-# }
